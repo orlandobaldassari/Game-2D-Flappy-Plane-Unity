321f0e1244913d574b49d45397e21aede4ef046c
Orlando Baldassari <obaldassarijr@hotmail.com>
2024-07-28 22:33:29 -0300

    Versão 1.3
    * Adicionado restart ao clicar na tela após game over;
    * Adicionado audio da pontuação;
    * Organizados assets;
    * Adicionada animação ao avião.

--- Assets/Scripts/Aviao.cs
+++ Assets/Scripts/Aviao.cs
@@ -5,20 +5,12 @@ using UnityEngine;
 public class Aviao : MonoBehaviour {
     private Rigidbody2D fisica;
     [SerializeField]
-    private float forca;
-    private Diretor diretor;
-    private Vector3 posicaoInicial;
-    
+    private float forca = 6;
 
     private void Awake()
     {
-        this.posicaoInicial = this.transform.position;
         this.fisica = this.GetComponent<Rigidbody2D>();
-    }
 
-    private void Start()
-    {
-        this.diretor = GameObject.FindObjectOfType<Diretor>();
     }
 
     private void Update () {
@@ -29,21 +21,9 @@ public class Aviao : MonoBehaviour {
 		
 	}
 
-    public void Reiniciar()
-    {
-        this.transform.position = this.posicaoInicial;
-        this.fisica.simulated = true;
-    }
-
     private void Impulsionar()
     {
         this.fisica.velocity = Vector2.zero;
         this.fisica.AddForce(Vector2.up * this.forca, ForceMode2D.Impulse);
-    }
-
-    private void OnCollisionEnter2D(Collision2D colisao)
-    {
-        this.fisica.simulated = false;
-        this.diretor.FinalizarJogo();
     }
 }
